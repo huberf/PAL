a92e48ec071bad4b521dca231a0f532d1f1cab40
Noah Huber-Feely <nhuberfeely@gmail.com>
2018-12-15 20:18:28 -0500

    fix Strava class

--- strava.py
+++ strava.py
@@ -4,16 +4,16 @@ class StravaData:
     RUNNER_ID = None
     ACCESS_TOKEN = None
     client = None
-    def __init__(runner_id, access_token):
+    def __init__(self, runner_id, access_token):
         RUNNER_ID = runner_id
         ACCESS_TOKEN = access_token
         client = Client()
         client.access_token = access_token
 
-    def getAthlete():
+    def getAthlete(self):
         athlete = client.get_athlete()
         return athlete
 
-    def getActivites():
+    def getActivites(self):
         activities = client.get_activities()
         return activities
